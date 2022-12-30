d1e66f6217fc04af1a19123d4ec09ff36389b17b
Charlotte <you@example.com>
2022-12-30 17:38:31 +0100

    V1 horaire fini

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,12 @@ public class Main {
 //        monAgenda.deleteJour("29/12/2022");
 //        monAgenda.getJour("29/12/2022");
         Session maSession = new Session("08:00", "09:00");
-
         monAgenda.addSession("29/12/2022","10:00", maSession ); //Fait la même chose que la ligne suivante mais en plus propre
         monAgenda.calendrier.get("29/12/2022").addSession("08:00", maSession); // Fait la même chose que la ligne d'avant mais en moins propre
-
-
-
         monAgenda.calendrier.get("29/12/2022").getSession("08:00");
         monAgenda.calendrier.get("29/12/2022").getSession("10:00");
 
+        //Commentaire ajouté par Richard
     }
     //Creation de la session "Manger" (11h - 13h) -> Blockée, pas possible de prévoir des activités
 
